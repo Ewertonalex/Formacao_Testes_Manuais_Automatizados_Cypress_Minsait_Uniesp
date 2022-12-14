a8e38ee641748928aaaf4f5d6e85d13c0aada208
Ewertonalex <ewertonlgk@gmail.com>
2022-12-14 15:43:59 -0300

    readme

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
+    
+
+    //conectando projeto ao dashboard do cypress:
+    projectId: "3n78ah",
+
+     
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    "chromeWebSecurity":false
-    
   },
 });
